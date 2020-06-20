ea59d7dc94eacd4165a3daa3958892dd00068f17
VergilWu <vergilcat@gmail.com>
2020-06-20 17:54:04 +0800

    fix

--- upload.py
+++ upload.py
@@ -15,13 +15,12 @@ def find_file(file_dir):
     return L
 
 
-time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-
 if not os.path.exists("pics_path.txt"):
     all_file = open('pics_path.txt', 'w')
     for line in find_file('D:\\Projects\\Desktop'):  # 查询路径
         all_file.writelines(line + '\n')
     all_file.close()
+    print("查询成功！请再次运行")
 else:
     APIKey = "xxxxx"  # 图床api
     url = "http://xxxxx/api/1/upload/?key=" + APIKey + "&format=json"  # 域名
@@ -33,7 +32,7 @@ else:
         if json.loads(r.text)["status_code"] == 200:
             uploaded_file = open('成功列表.txt', 'a')
             uploaded_file.writelines(
-                '\n' + time + '\n' + all_line + '\n' + json.loads(r.text)["image"][
+                '\n' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '\n' + all_line + '\n' + json.loads(r.text)["image"][
                     "url"] + '\n' + '--------------' + '\n')
             uploaded_file.close()
 
@@ -45,12 +44,12 @@ else:
                         continue
                     f_w.write(line)
 
-            print(time + '\n' + "上传成功: " + all_line)
+            print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '\n' + "上传成功: " + all_line)
 
             continue
         else:
             upload_fail_file = open('失败列表.txt', 'a')
-            upload_fail_file.writelines(time + '\n' + all_line + '\n')
+            upload_fail_file.writelines(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '\n' + all_line + '\n')
             upload_fail_file.close()
 
-            print(time + '\n' + "上传失败: " + all_line)
+            print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + '\n' + "上传失败: " + all_line)
